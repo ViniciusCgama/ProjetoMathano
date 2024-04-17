5650d8e0a1de350326834d74ab15b58faf940e34
Vinicius Coelho Gama <128941564+ViniciusCgama@users.noreply.github.com>
2024-04-17 16:51:46 -0300

    First commit

---
+++ backend/controllers/UserController.js
@@ -0,0 +1,71 @@
+const User = require('../models/User');
+
+module.exports = class UserController {
+    static async register(req, res) {
+        const { name, email, phone, password, confirmpassword } = req.body;
+    
+        //validations
+        if(!name) {
+            res.status(422).json({ message: 'O nome é obrigatório'})
+            return
+        }
+
+        if(!email) {
+            res.status(422).json({ message: 'O email é obrigatório'})
+            return
+        }
+
+        if(!phone) {
+            res.status(422).json({ message: 'O telefone é obrigatório'})
+            return
+        }
+
+        if(!password) {
+            res.status(422).json({ message: 'A senha é obrigatório'})
+            return
+        }
+
+        if(!confirmpassword) {
+            res.status(422).json({ message: 'É nescessario confirmar a senha'})
+    }
+
+         if(password !== confirmpassword) {
+        res.status(422).json({ message: 'É nescessario que a confirmação seja igual a senha'})
+        return
+        }
+
+        //check if user exists
+        const userExists = await User.findOne({ email: email }) 
+
+        if(userExists) {
+            res.status(422).json({ 
+                message: 'Por favor, utilize outro e-mail',
+            })
+            return
+    }
+
+        // create a password
+        const salt = await bcrypt.genSalt(12)
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        // create a user
+        const user = new User({
+            name,
+            email,
+            phone,
+            password: passwordHash,
+        })
+
+        try {
+        const newUser = await user.save()
+            res.status(201).json({    
+                message: 'Usuário criado',
+                newUser,
+            })
+
+        } catch(error) {
+            res.status(500).json({message: error})
+        }
+
+}
+}
